bb201b8ff5edc599a9827e3e1e07601422dd9213
Ronny <ronnymedina63@gmail.com>
2021-03-28 22:20:55 +0200

    mvc frontal

---
+++ view/layout/footer.php
@@ -0,0 +1,78 @@
+<!--Peu de la pagina -->
+        <footer id="footer" class="text-center text-lg-start mt-5">
+            <div class="row mx-auto p-5"> 
+                <!-- Grid colummn -->
+                <div class="col-lg-3 col-md-12 mb-4 mb-md-0">         
+                    <!-- Logo -->
+                    <a href="index.php">
+                        <img src="#" alt="logo" />
+                    </a>
+                    <p>
+                        El teu lloc ideal per publicar els teus llibres per a escriptors i gaudir d'una àmplia gamma d'històries per a lectors.
+                    </p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-4 mb-4 mb-md-0">
+                    <h5>Informació</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!">Avis legal</a>
+                        </li>
+                        <li>
+                            <a href="#!">Politica de cookies</a>
+                        </li>
+                        <li>
+                            <a href="#!" >Politica de privacitat</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-4 mb-4 mb-md-0">
+                    <h5>Compte</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">El meu compte</a>
+                        </li>
+                        <li>
+                            <a href="#!">Els meus preferits</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-4 mb-4 mb-md-0">
+                    <h5>Segueix-nos</h5>
+                    <div id="social">
+                        <i class="fab fa-facebook-square"></i>
+                        <i class="fab fa-twitter-square"></i>
+                        <i class="fab fa-instagram-square"></i>
+                        <i class="fab fa-google-plus-square"></i>
+                    </div>
+                        
+                </div>
+                <!--Grid column-->    
+            </div> 
+
+            <div class="row">
+                <div class="col-lg-12 p-0">
+                    <!-- Copyright -->
+                    <div id="copyright" class="text-center p-3">
+                        &COPY <?= date('Y') ?> | 
+                        <a href="index.php">write&read.com</a>
+                    </div>
+                    <!-- Copyright -->
+                </div>
+            </div>
+                
+        </footer>
+        <!-- Fi peu de pagina -->
+    </div>
+</body>
+</html>
