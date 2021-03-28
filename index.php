fc877a3dbc85c6dec534f1340fb5c4c5950e9ffe
Ronny <ronnymedina63@gmail.com>
2021-03-28 10:50:04 +0200

    .

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="ca">
 <head>
     <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Plataforma d'escriptors amateurs">
+    <meta name="keywords" content="escriptors, llibres">
+    <meta name="author" content="demateu, ronny, apossai">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Write&Read</title>
 
     <!-- Font awesome CDN -->
