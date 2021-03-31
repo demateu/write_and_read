91320f6d20f9712b2d11673b43fa9401b0fe4d2b
Ronny <ronnymedina63@gmail.com>
2021-03-31 18:57:09 +0200

    canvis al front controller

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'autoload.php';
-
 require_once 'view/layout/header.php';
 require_once 'view/layout/main.php';
 
@@ -17,30 +16,28 @@ require_once 'view/layout/main.php';
 //compruebo si me llega el controlador por la url
 if (isset($_GET['controller'])) {
     //si me llega, genero esta variable
-    $nombre_controlador = $_GET['controller'];
+    $nombre_controlador = $_GET['controller'].'Controller';
+
+    //compruebo si existe el controlador, la clase
+    if (class_exists($nombre_controlador)) {
+        //si existe la clase, instancio el objeto
+        $controlador = new $nombre_controlador();
+
+        //si el metodo existe, llamo al metodo
+        if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+            //recoje la accion desde la url por GET
+            $action = $_GET["action"];
+            //llama al metodo que pide la url
+            $controlador->$action();
+        } else {
+            echo "La pagina que buscas no existe1";
+        }
+    } else {
+        echo "La pagina que buscas no existe2";
+    }
 } else {
     //si no llega lanza mensaje de error y corta la ejecucion
     echo "La pagina que buscas no existe";
-    exit(); //para la ejecucion y lo de abajo ya no lo ejecuta
 }
-
-//compruebo si existe el controlador, la clase
-if (class_exists($nombre_controlador)) {
-    //si existe la clase, instancio el objeto
-    $controlador = new $nombre_controlador();
-
-    //si el metodo existe, llamo al metodo
-    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
-        //recoje la accion desde la url por GET
-        $action = $_GET["action"];
-        //llama al metodo que pide la url
-        $controlador->$action();
-    } else {
-        echo "La pagina que buscas no existe1";
-    }
-} else {
-    echo "La pagina que buscas no existe2";
-}
-
 
 require_once 'view/layout/footer.php';
