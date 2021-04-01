be12cd02062f42a469b30a2c410aad8431ad9b78
Monica Mateu <demateu@MacBook-Pro.local>
2021-04-01 17:40:44 +0200

    añado getters y setters al modelo usuari

--- model/Usuari.php
+++ model/Usuari.php
@@ -6,6 +6,8 @@
  * de moment ho poso tot juntç
  * 
  * seran propiedades privadas porque solo podremos acceder a ellos mediante metodos
+ * 
+ * se usará para crear objetos
  */
 class Usuari{
 
@@ -22,6 +24,246 @@ class Usuari{
     private $id_tipususuari; //FK
     private $biografia;//nomes per escriptors
 
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nickname
+     */ 
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    /**
+     * Set the value of nickname
+     *
+     * @return  self
+     */ 
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nom_i_cognoms
+     */ 
+    public function getNom_i_cognoms()
+    {
+        return $this->nom_i_cognoms;
+    }
+
+    /**
+     * Set the value of nom_i_cognoms
+     *
+     * @return  self
+     */ 
+    public function setNom_i_cognoms($nom_i_cognoms)
+    {
+        $this->nom_i_cognoms = $nom_i_cognoms;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dni
+     */ 
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * Set the value of dni
+     *
+     * @return  self
+     */ 
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of data_alta
+     */ 
+    public function getData_alta()
+    {
+        return $this->data_alta;
+    }
+
+    /**
+     * Set the value of data_alta
+     *
+     * @return  self
+     */ 
+    public function setData_alta($data_alta)
+    {
+        $this->data_alta = $data_alta;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of avatar_id
+     */ 
+    public function getAvatar_id()
+    {
+        return $this->avatar_id;
+    }
+
+    /**
+     * Set the value of avatar_id
+     *
+     * @return  self
+     */ 
+    public function setAvatar_id($avatar_id)
+    {
+        $this->avatar_id = $avatar_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of subscrit
+     */ 
+    public function getSubscrit()
+    {
+        return $this->subscrit;
+    }
+
+    /**
+     * Set the value of subscrit
+     *
+     * @return  self
+     */ 
+    public function setSubscrit($subscrit)
+    {
+        $this->subscrit = $subscrit;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of data_naixement
+     */ 
+    public function getData_naixement()
+    {
+        return $this->data_naixement;
+    }
+
+    /**
+     * Set the value of data_naixement
+     *
+     * @return  self
+     */ 
+    public function setData_naixement($data_naixement)
+    {
+        $this->data_naixement = $data_naixement;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_tipususuari
+     */ 
+    public function getId_tipususuari()
+    {
+        return $this->id_tipususuari;
+    }
+
+    /**
+     * Set the value of id_tipususuari
+     *
+     * @return  self
+     */ 
+    public function setId_tipususuari($id_tipususuari)
+    {
+        $this->id_tipususuari = $id_tipususuari;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of biografia
+     */ 
+    public function getBiografia()
+    {
+        return $this->biografia;
+    }
+
+    /**
+     * Set the value of biografia
+     *
+     * @return  self
+     */ 
+    public function setBiografia($biografia)
+    {
+        $this->biografia = $biografia;
+
+        return $this;
+    }
 }
 
 /*
