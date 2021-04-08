89573d837789589dc76ccbd3e7b7d5cf197bd99a
Monica Mateu <demateu@MacBook-Pro.local>
2021-04-08 18:40:20 +0200

    añado logo, favicon, rectifico flechas de los sliders para que se vuelvan a mover

--- view/layout/header.php
+++ view/layout/header.php
@@ -9,28 +9,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Write&Read</title>
 
-    <!-- Favicon
-    <link rel="icon" href="demo_icon.gif" type="image/gif" sizes="16x16">
-    
+    <link rel="icon" href="<?=base_url?>assets/img/identity/favicon_99px.ico">
+    <!-- Favicon    
+    <link rel="icon" href="../../assets/img/identity/wr_favicon_32px.png" type="image/png" sizes="32x32">
     Para incluir un favicon en formato .ico , debemos utilizar la siguiente línea:
     <link rel="icon" type="image/x-icon" href="/RUTA/FICHERO_FAVICON.ico">
     Para incluir un favicon en formato .gif , debemos utilizar la siguiente línea:
     <link rel="icon" type="image/gif" href="/RUTA/FICHERO_FAVICON.gif">
     Para incluir un favicon en formato .png , debemos utilizar la siguiente línea:
-    <link rel="icon" type="image/png" href="/RUTA/FICHERO_FAVICON.png">
-    -->
+    <link rel="icon" type="image/png" href="../../assets/img/identity/wr_favicon_32px.png">-->
+    
 
     <!-- Font awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
-    <!--Bootstrap CDN-->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
     <!-- CSS 
     script para que vaya cargando version nueva y solucione el almacenado en cache-->
     <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time(); ?>" />
-    <script src="/js/functions.js?v=<?php echo time(); ?>"></script>
+
 </head>
 
 <body>
@@ -56,7 +55,7 @@
                 <!--Logo i buscador de la web -->
                 <div class="col-2" id="logo_search">
                     <a href="<?=base_url?>index.php">
-                        <img src="<?=base_url?>#" alt="logo" />
+                        <img src="<?=base_url?>assets/img/identity/wr_logo_250px.png" alt="logo" width="250" height="auto"/>
                     </a>
                 </div>
                 <!--  MENU  -->
