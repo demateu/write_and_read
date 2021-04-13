ebc9b9bd23147411e22de8c2f5a69e22a47c45ed
Monica Mateu <demateu@MacBook-Pro.local>
2021-04-13 18:26:25 +0200

    arreglo estilos escriptor fitxa

--- view/panel_control/EscriptorView.php
+++ view/panel_control/EscriptorView.php
@@ -5,13 +5,13 @@
 <!--redissenyat demateu-->
 <div class="container-fluid">
 
-    <section class="row justify-content-center" id="usuari-section">
-        <div class="col-4">
+    <section class="row justify-content-center p-dark" id="usuari-section">
+        <div class="col-6">
             <div class="row justify-content-center">
-                <div class="col-4 imatge-avatar">
+                <div class="col imatge-avatar" id="imatge-avatar">
                     <!--imatge de l'escriptor (avatar)-->
                     <?php if(isset($id)): ?>
-                        <img src="<?=$escriptor->avatar_url_imagen?>" alt="Imatge de <?=$escriptor->nom_i_cognoms?>" class="card-img-top" />
+                        <img src="<?=$escriptor->avatar_url_imagen?>" alt="Imatge de <?=$escriptor->nom_i_cognoms?>" class="card-img-top mx-auto d-block" />
                     <?php else: ?>
                         <h1 class="text-center">L'escriptor no existeix</h1>
                     <?php endif; ?>               
@@ -19,10 +19,10 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col">
+                <div class="col" id="nom-complert-avatar">
                     <!--Nom i cognoms de l'escriptor-->
                     <?php if(isset($id)): ?>
-                        <h1 class="text-center"><?=$escriptor->nom_i_cognoms?></h1>
+                        <h1 class="text-center p-2"><?=$escriptor->nom_i_cognoms?></h1>
                     <?php else: ?>
                         <h1 class="text-center">L'escriptor no existeix</h1>
                     <?php endif; ?>
@@ -30,19 +30,76 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-6">
+                <div class="col" id="nickname-avatar">
                     <!--Nickname de l'escriptor-->
                     <?php if(isset($id)): ?>
-                        <p class="card-title">@<?=$escriptor->nickname?> | <?=$escriptor->data_naixement?></p>
+                        <h2 class="card-title text-center">@<?=$escriptor->nickname?> | <?=$escriptor->data_naixement?></h2>
                     <?php else: ?>
-                        <p class="card-title">L'escriptor no existeix</p>
+                        <h2 class="card-title">L'escriptor no existeix</h2>
                     <?php endif; ?>
                     <!--fi nickname-->                    
                 </div>
             </div>
-            <div>
+            <div class="row justify-content-center">
+                <div class="col text-justify" id="biografia_avatar">
+                    <!--Biografia de l'escriptor-->
+                    <?php if(isset($id)): ?>
+                        <p class="card-text text-center"><?=$escriptor->biografia?></p>                    
+                    <?php else: ?>
+                        <p class="card-title text-justify"><strong>Data de Naixement: </strong>L'escriptor no existeix</p>
+                    <?php endif; ?>
+                    <!--fi biografia-->                        
+                </div>
+            </div>
 
         </div>
+    </section>
+
+    <section class="row" id="llibres_publicats">
+        <div class="col-10 mx-auto">
+            <div class="row">
+                <div class="col">
+                    <h2 class="text-center mb-5">Llibres Publicats</h2>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-sm-6 col-lg-3">
+                    <div class="card">
+                        <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
+                        <div class="card-body">
+                            <h4 class="card-title">Don Quijote de la Mancha</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="card">
+                        <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
+                        <div class="card-body">
+                            <h4 class="card-title">Don Quijote de la Mancha</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="card">
+                        <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
+                        <div class="card-body">
+                            <h4 class="card-title">Don Quijote de la Mancha</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="card">
+                        <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
+                        <div class="card-body">
+                            <h4 class="card-title">Don Quijote de la Mancha</h4>
+                        </div>
+                    </div>
+                </div>            
+
+            </div>
+        </div>
+
     </section>
 
 </div>
@@ -53,56 +110,8 @@
 
 
 
-<!--Contingut principal -->
-<main>
-    <!--Nom i cognoms de l'escriptor-->
-    <?php if(isset($id)): ?>
-        <h1 class="text-center"><?=$escriptor->nom_i_cognoms?></h1>
-    <?php else: ?>
-        <h1 class="text-center">L'escriptor no existeix</h1>
-    <?php endif; ?>
-    <!--FI nom i cognoms-->
-    <div class="container border border-secondary">
+<!--Contingut antic -->
         <div class="row">
-            <div class="col-md-auto imatge-avatar">
-                <!--imatge de l'escriptor (avatar)-->
-                <?php if(isset($id)): ?>
-                    <img src="<?=$escriptor->avatar_url_imagen?>" alt="Imatge de <?=$escriptor->nom_i_cognoms?>" class="card-img-top" />
-                <?php else: ?>
-                    <h1 class="text-center">L'escriptor no existeix</h1>
-                <?php endif; ?>               
-                <!--fi imatge-->
-            </div>
-            <div class="col"><br>
-                <!--Nickname de l'escriptor-->
-                <?php if(isset($id)): ?>
-                    <h4 class="card-title" style="color:#925279"><?=$escriptor->nickname?></h1>
-                <?php else: ?>
-                    <h4 class="card-title" style="color:#925279">L'escriptor no existeix</h1>
-                <?php endif; ?>
-                <!--fi nickname-->
-                <!--Data de naixement de l'escriptor-->
-                <?php if(isset($id)): ?>
-                    <p class="card-title"><strong>Data de Naixement: </strong><?=$escriptor->data_naixement?></p>
-                <?php else: ?>
-                    <p class="card-title"><strong>Data de Naixement: </strong>L'escriptor no existeix</p>
-                <?php endif; ?>
-                <!--fi data de naixement-->
-                <hr>
-                <h4 class="card-title">Biografia</h4>
-                <!--Biografia de l'escriptor-->
-                <?php if(isset($id)): ?>
-                    <p class="card-text"><?=$escriptor->biografia?></p>                    
-                <?php else: ?>
-                    <p class="card-title"><strong>Data de Naixement: </strong>L'escriptor no existeix</p>
-                <?php endif; ?>
-                <!--fi biografia-->
-            </div>
-        </div>
-
-
-        <div class="row">
-
                 <!-- Fitxa de llibre -->
                 <div class="col-sm-6 col-lg-3">
                     <div class="card">
@@ -153,50 +162,9 @@
                     </div>
                 </div>
                 <!-- fi fitxa -->
-
         </div>
+<!--fi contingut antic-->
 
 
-        <!--LLIBRES PUBLICATS-->
-        <section class="row" id="llibres_publicats">
-            <h4 class="card-title text-center">Llibres Publicats</h4>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
-                    <div class="card-body">
-                        <h4 class="card-title">Don Quijote de la Mancha</h4>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
-                    <div class="card-body">
-                        <h4 class="card-title">Don Quijote de la Mancha</h4>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
-                    <div class="card-body">
-                        <h4 class="card-title">Don Quijote de la Mancha</h4>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="https://www.elejandria.com/covers/Don_Quijote_de_la_Mancha-Cervantes_Miguel-md.png" alt="Imatge del llibre" class="card-img-top" />
-                    <div class="card-body">
-                        <h4 class="card-title">Don Quijote de la Mancha</h4>
-                    </div>
-                </div><br>
-            </div>
-        </section>
-        <!--FI LLIBRES PUBLICATS-->
-
-
-    </div>
-</main>
 
 <?php require_once 'view/layout/footer.php';?>
